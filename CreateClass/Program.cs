17cbef6dddfd57e1894f84498ffd3ff8eef9bf43
jhon <464479460@qq.com>
2017-05-08 21:38:23 +0800

    Just test function

--- CreateClass/Program.cs
+++ CreateClass/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CreateClass
 {
@@ -10,6 +6,7 @@ namespace CreateClass
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Test");
         }
     }
 }
